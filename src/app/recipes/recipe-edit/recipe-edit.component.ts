5402cad07ba6ee745741d58867ebef30c634ca2c
Nico Neiman <nico@farmathand.com>
2021-02-18 17:01:44 -0600

    new recipe and edit comp

---
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -0,0 +1,44 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {ActivatedRoute, Params} from '@angular/router';
+import {RecipesService} from '../recipes.service';
+import {Ingredient} from '../../shared/ingredients.model';
+import {Recipe} from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-edit',
+  templateUrl: './recipe-edit.component.html',
+  styleUrls: ['./recipe-edit.component.css']
+})
+export class RecipeEditComponent implements OnInit {
+  id: number;
+  editMode = false;
+  name = '';
+  description = '';
+  ingredients: Ingredient[] = [];
+  imagePath = '';
+
+  constructor( private routes: ActivatedRoute,
+               private recipeService: RecipesService ) { }
+
+  ngOnInit(): void {
+    this.routes.params
+      .subscribe(
+        (params: Params) => {
+          this.id = + params.id;
+          this.editMode = !!params.id;
+          if (!!this.editMode) {
+            this.name = this.recipeService.getRecipe(this.id).name;
+            this.description = this.recipeService.getRecipe(this.id).description;
+            this.ingredients = this.recipeService.getRecipe(this.id).ingredients;
+            this.imagePath = this.recipeService.getRecipe(this.id).imagePath;
+          }
+        }
+      );
+  }
+
+  onSave(): void {
+    const recipe: Recipe = new Recipe( this.name, this.description, this.imagePath, this.ingredients);
+    this.recipeService.getRecipes()[this.id] = recipe;
+  }
+
+}
